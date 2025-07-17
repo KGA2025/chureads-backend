3f1cd263a86ad2b8ba56d32eac9d04413a97c529
KGA2025 <gn330gn@naver.com>
2025-07-17 15:09:59 +0900

    feat : post CRUD 구현

---
+++ database/db.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+
+let db = null;
+
+export const connectDB = async () => {
+  try {
+    // 이미 DB가 연결되어 있는 경우
+    if (db) return db;
+
+    // DB 연결 후 해당 DB 반환
+    const MONGODB_URI = process.env.MONGODB_URI_LOCAL;
+    const client = new MongoClient(MONGODB_URI);
+    await client.connect();
+    db = client.db(process.env.DB_NAME);
+    console.log("🔌MongoDB 연결 성공!");
+
+    return db;
+  } catch (error) {
+    console.log("❌MongoDB 연결 실패:", error);
+    process.exit(1); // 프로그램 강제 종료(1 : 오류를 의미)}
+  }
+};
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv"; // 백엔드에서는 .env에 접근하기 위해서는 dotenv 필요
 import { testTagGenerate } from "./services/tagService.js";
-import postRouter from "./routes/posts.js";
+import postRouter, { init } from "./routes/posts.js";
+import { connectDB } from "./database/db.js";
 
 // 환경변수 로드
 // 전역으로 로드해서 모든 node.js 모듈 내에서 접근 가능
@@ -17,6 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 // 라우터 미들웨어 등록
 app.use("/posts", postRouter); // 요청받는 api의 앞 부분이 posts인 경우
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log("Server at..", PORT);
+  const db = await connectDB();
+  init(db);
 });
